5127253aa84cae172c94e4401555fbcf991dbf15
Chris <Chris@eventix.nl>
2019-08-13 12:28:53 +0200

    Implement company specific cache

--- Translator.php
+++ Translator.php
@@ -5,17 +5,19 @@ namespace Eventix\Translation;
 class Translator extends \Illuminate\Translation\Translator {
 
     protected $differential = null;
+    protected $diffCache = [];
 
     public function load($namespace, $group, $locale, $differential = null) {
         $diff = $differential ?? $this->differential;
-        if ($this->isLoaded($namespace, $group, $locale, $diff)) {
-            return;
+        if (array_key_exists($diff, $this->diffCache)) {
+            $this->loaded[$namespace][$group][$locale] = $this->diffCache[$diff];
+        }else{
+            // The loader is responsible for returning the array of language lines for the
+            // given namespace, group, and locale. We'll set the lines in this array of
+            // lines that have already been loaded so that we can easily access them.
+            $lines = $this->loader->load($locale, $group, $namespace, $diff);
+            $this->loaded[$namespace][$group][$locale] = $this->diffCache[$diff] = $lines;
         }
-        // The loader is responsible for returning the array of language lines for the
-        // given namespace, group, and locale. We'll set the lines in this array of
-        // lines that have already been loaded so that we can easily access them.
-        $lines = $this->loader->load($locale, $group, $namespace, $diff);
-        $this->loaded[$namespace][$group][$locale] = $lines;
     }
 
     /**
@@ -26,20 +28,5 @@ class Translator extends \Illuminate\Translation\Translator {
      */
     public function setDifferential($differential) {
         $this->differential = $differential;
-    }
-
-    /**
-     * Get all lines loaded in this translator
-     *
-     * @param $locale The locale to load the translation of
-     * @param $group The translation group
-     * @param null $namespace The namespace to load
-     * @param bool $differential The differential to load
-     * @return mixed All translated lines
-     */
-    public function getLines($locale, $group, $namespace = null, $differential = false) {
-        $this->load($namespace, $group, $locale, $differential);
-
-        return $this->loaded[$namespace][$group][$locale];
     }
 }
